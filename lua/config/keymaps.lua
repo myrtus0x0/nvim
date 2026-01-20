e31cb70f3c9b6791eb730a255738fd9234efcf9b
Myrtus <myrtus0x0@protonmail.com>
2026-01-20 11:34:29 -0800

    updated keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Floaterm keybindings (replaces built-in terminal)
+vim.keymap.set("n", "<C-/>", "<cmd>FloatermToggle<cr>", { desc = "Toggle Floaterm" })
+vim.keymap.set("t", "<C-/>", "<cmd>FloatermToggle<cr>", { desc = "Toggle Floaterm" })
+vim.keymap.set("n", "<leader>ft", "<cmd>FloatermToggle<cr>", { desc = "Toggle Floaterm" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.scrolloff = 8 -- Keep 8 lines visible above/below cursor
+vim.opt.colorcolumn = "80" -- Visual guide at 80 chars
+vim.opt.conceallevel = 0 -- Show markdown/json syntax (useful for security work)
