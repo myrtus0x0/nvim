ca9b12dc785eddb5e789a9bc23472a71ac41067f
Myrtus <myrtus0x0@protonmail.com>
2026-05-27 16:11:21 -0700

    removed obsidian

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,3 @@
 vim.opt.relativenumber = true -- Relative line numbers
 vim.opt.scrolloff = 8 -- Keep 8 lines visible above/below cursor
 vim.opt.wrap = false
-vim.opt.conceallevel = 1 -- Conceal markup for obsidian.nvim UI
